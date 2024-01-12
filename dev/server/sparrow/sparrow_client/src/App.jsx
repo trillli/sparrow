2a2d34e3d0465aa341579c5adb5940df2d0c3130
TJ <tedwardj11@gmail.com>
2024-01-12 14:54:50 -0500

    pin; initial react dependency prototype setup and working

--- dev/server/sparrow/sparrow_client/src/App.jsx
+++ dev/server/sparrow/sparrow_client/src/App.jsx
@@ -2,5 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TrillliApp from 'trillli_client/src/TrillliApp'
+
+function App() {
+  return (
+    <TrillliApp app_name='Sparrow' />
+  )
+}
 
 export default App
