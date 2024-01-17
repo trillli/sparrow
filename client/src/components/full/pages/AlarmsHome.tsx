0488fe626f55473303beee57505f131ec482e39a
TJ <tedwardj11@gmail.com>
2024-01-17 14:06:09 -0500

    pin; setting up top and left nav

--- client/src/components/full/pages/AlarmsHome.tsx
+++ client/src/components/full/pages/AlarmsHome.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Button, Link, Container, Typography } from '@mui/material';
+import { Container } from '@mui/material';
 import AlarmsList from 'src/components/full/elements/AlarmsList';
 import ConfigCategoryLabel from 'src/components/full/elements/ConfigCategoryLabel';
 import DeviceConfig from 'src/components/full/elements/DeviceConfig';
@@ -12,7 +12,7 @@ const AlarmsHome = () => {
 
     return (
         <div>
-            <Typography>Your alarms</Typography>
+            <h2>Your alarms</h2>
             <AlarmsList />
             <Container>
                 <ConfigCategoryLabel />
