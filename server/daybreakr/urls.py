73cfb48098b92ac7e053761517760b5a30f97ffb
TJ <tedwardj11@gmail.com>
2024-02-25 17:20:32 -0500

    pin; serverside code reorg good

--- server/daybreakr/urls.py
+++ server/daybreakr/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    
-    path('tr/', include('tr_admin.urls')),
+    path('tr/', include('trillli.urls')),
     path('api/messages/', include('daybreakr_data.urls'))
 ]
 
