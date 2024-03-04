676bc84106bb3396fd6e6b6d067d37bad36a445d
TJ <tedwardj11@gmail.com>
2024-03-03 21:48:53 -0500

    pin; auth back to working state

--- server/daybreakr_data/models.py
+++ server/daybreakr_data/models.py
@@ -14,5 +14,5 @@ class Metadata:
 class LazyAlarm(models.Model):
     user_id = models.CharField(max_length=512, primary_key=True)
     alarms_json = models.TextField()
-    other = models.TextField()
+    # other = models.TextField()
     # updated = models.DateTimeField(auto_now = True, blank = True)
