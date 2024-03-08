fe9685ced32677d8170165001286d1f155f6dc27
TJ <tedwardj11@gmail.com>
2024-03-07 21:26:37 -0500

    pin; cleanup of auth0 helper lib

--- server/tr_common/auth0_helper.py
+++ server/tr_common/auth0_helper.py
@@ -12,17 +12,14 @@ class Auth0Helper:
         super().__init__()
         self.ex_helper = ExceptionHelper()
 
+    #Prepares and returns information required to make request to auth0 management api
     def _configure_management_api_request(self, request):
 
         try:
 
-            #todo: look into get id from request
             user_id_with_sub = self._get_sub_claim_from_request(request)
             management_api_access_token = self._get_management_api_access_token()
             auth0_domain = settings.AUTH0_DOMAIN
-
-            # print('auth0_domain:')
-            # print(auth0_domain)
 
             api_request_setup = {
                 'domain': auth0_domain,
@@ -33,17 +30,15 @@ class Auth0Helper:
             return api_request_setup
 
         except Exception as e:
-            print('in exception; need to handle')
-            traceback.print_exc()
+            raise(e)
 
         
 
         
 
-    '''
-    - Returns a serialized representation of the normalized user profile
-    - Deserialize to python dict with user.json()
-    '''
+    
+    #Returns a serialized representation of the normalized user profile
+    #Deserialize to python dict with user.json()
     def management_api_user_get(self, request):
         
         #Pass request object to management api request config helper method
@@ -59,6 +54,7 @@ class Auth0Helper:
         
         return user
     
+    #Updates a user's provide via the management api
     def management_api_user_patch(self, request, payload):
 
         #Pass request object to management api request config helper method
@@ -71,45 +67,36 @@ class Auth0Helper:
                 'Authorization': 'Bearer ' + api_request_setup['access_token'],
                 'content-type': 'application/json'
             }
-        # user = requests.get(url, headers=headers)
-        # user = requests.patch(url, headers=headers, data=payload)
+        
         response = requests.request("PATCH", url, headers=headers, data=payload)
 
         return response
     
+    #Triggers the auth0-managed user password reset flow
     def management_api_password_change(self, request):
-        print('test')
+
+        domain = settings.AUTH0_DOMAIN
         clientId = settings.AUTH0_CLIENT_ID
         user = self.management_api_user_get(request)
         email = user.json()['email']
-        print('email is:')
-        print(email)
-        print('AND HERE I AM IN THE SPARROW ONE, THATS GOOD')
 
-        url = 'https://dev-m0zbh7x7q4v4tlo6.us.auth0.com/dbconnections/change_password'
+        url = 'https://' + domain + '/dbconnections/change_password'
         headers = headers = { 'content-type': "application/json" }
-        # payload = "{\"client_id\": \"vYABLvf58qGhCj2kARAv3HnFsJ4vpeep\",\"email\": \"" + email + "\",\"connection\": \"Username-Password-Authentication\"}"
         payload = "{\"client_id\": \"" + clientId + "\",\"email\": \"" + email + "\",\"connection\": \"Username-Password-Authentication\"}"
 
-        print(payload)
-
         response = requests.post(url, headers=headers, data=payload)
-        print('made it past the change password api call response:')
-        print(response)
-        print(response.json)
 
         return response
     
 
-    '''
-        https://auth0.com/docs/secure/tokens/access-tokens/management-api-access-tokens
-    '''
+    
+    # https://auth0.com/docs/secure/tokens/access-tokens/management-api-access-tokens
     def _get_management_api_access_token(self):
-        url = "https://dev-m0zbh7x7q4v4tlo6.us.auth0.com/oauth/token"
+        url = 'https://' + settings.AUTH0_DOMAIN + '/oauth/token'
         payload = {
             "client_id": settings.AUTH0_MANAGEMENT_API_CLIENT_ID,
             "client_secret": settings.AUTH0_MANAGEMENT_API_CLIENT_SECRET,
-            "audience": "https://dev-m0zbh7x7q4v4tlo6.us.auth0.com/api/v2/",
+            "audience": 'https://' + settings.AUTH0_DOMAIN + '/api/v2/',
             "grant_type": "client_credentials"
         }
 
@@ -120,7 +107,8 @@ class Auth0Helper:
         if response.status_code == 200:
             management_api_response = response.json()
         else:
-            print('in response status code indicates error; need to handle')
+            msg = 'in response status code indicates error; need to handle'
+            raise(msg)
             # traceback.print_exc()
 
         return management_api_response.get('access_token')
@@ -136,9 +124,7 @@ class Auth0Helper:
 
         token = self._get_bearer_token_from_request(request)
         decoded_token = self._decode_jwt_bearer_token(token)
-        # print('before decodedtoken.get')
         sub = decoded_token.get('sub')  # Extract user ID from the token
-        # print('after decodedtoken.get')
 
         return sub
     
@@ -149,16 +135,12 @@ class Auth0Helper:
 
         token = self._get_bearer_token_from_request(request)
         decoded_token = self._decode_jwt_bearer_token(token)
-        # print('before decodedtoken.get')
         sub = decoded_token.get('sub')  # Extract user ID from the token
         user_id = self._parse_id_from_sub_claim(sub)
-        # print('after decodedtoken.get')
 
         return user_id
     
     def _parse_id_from_sub_claim(self, sub):
-        print('going to split:')
-        print(sub)
         subSplit = sub.split('|')
         return subSplit[1]
     
@@ -179,7 +161,6 @@ class Auth0Helper:
     '''
     def _get_bearer_token_from_request(self, request):
         
-        '''Get headers; pass to '''
         request_headers = request.headers
         token = self._get_bearer_token_from_dict(request_headers)
         
@@ -194,13 +175,8 @@ class Auth0Helper:
     https://auth0.com/docs/secure/tokens/access-tokens
     '''
     def _get_bearer_token_from_dict(self, dict):
-
-        # print('here in get bearer token from dict. dict is:')
-        # print(dict)
-        # print('just printed dict')
         
         authorization_header = dict.get('Authorization')
-        # print (authorization_header)
         token = authorization_header.split(' ')[1]
 
         return token
